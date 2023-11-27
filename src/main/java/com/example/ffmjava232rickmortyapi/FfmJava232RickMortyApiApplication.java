fcf7f7e883dd5e9898e86e87e39072816c3e6ccd
Ivan <i85chiosa@gmail.com>
2023-11-27 15:37:28 +0100

    Initial commit

---
+++ src/main/java/com/example/ffmjava232rickmortyapi/FfmJava232RickMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ffmjava232rickmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmJava232RickMortyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfmJava232RickMortyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ffmjava232rickmortyapi/FfmJava232RickMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ffmjava232rickmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmJava232RickMortyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
